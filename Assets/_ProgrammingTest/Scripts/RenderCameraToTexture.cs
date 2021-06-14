f44c10d1b28a5d168f92dee1c0daca2fa1f2d5f4
alplahe <albertoplazaherraiz@gmail.com>
2021-06-14 20:14:08 +0200

    Render to texture update

---
+++ Assets/_ProgrammingTest/Scripts/RenderCameraToTexture.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RenderCameraToTexture : MonoBehaviour
+{
+  Camera renderCamera;
+  public Texture texture;
+  public Material material;
+  public MeshRenderer meshRenderer;
+  RenderTexture renderTexture;
+  public RawImage rawImage;
+
+  private void Start()
+  {
+    Init();
+  }
+
+  void Init()
+  {
+    renderCamera = GetComponent<Camera>();
+    renderTexture = new RenderTexture(256, 256, 0);
+        
+    if (renderCamera == null) return;
+    if (rawImage == null) return;
+
+    //material = meshRenderer.materials[0];
+    texture = rawImage.texture;
+    CreateRenderedTexture();
+  }
+
+  void CreateRenderedTexture()
+  {
+    Debug.Log("CreateRenderedTexture");
+    renderCamera.targetTexture = (RenderTexture)rawImage.texture;
+    //renderCamera.targetTexture = (RenderTexture)material.GetTexture("RenderCamera");
+  }
+}
