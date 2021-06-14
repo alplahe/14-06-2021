c100ffbfec55768b819a496f5e8e3af41291b304
alplahe <albertoplazaherraiz@gmail.com>
2021-06-14 12:33:10 +0200

    Singleton implemented

---
+++ Assets/_ProgrammingTest/Scripts/FlowManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowManager : MonoBehaviour
+{
+  private static FlowManager instance;
+
+  public static FlowManager Instance 
+  { 
+    get 
+    { 
+      return instance; 
+    } 
+  }
+
+  void Awake()
+  {
+    if (instance != null && instance != this)
+    {
+      Debug.Log("Destroy duplicated FlowManager instance");
+      Destroy(this.gameObject);
+    }
+    else
+    {
+      Debug.Log("Creating FlowManager instance");
+      instance = this;
+      DontDestroyOnLoad(instance);
+    }
+  }
+}
